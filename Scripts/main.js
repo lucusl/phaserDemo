0c3cb12b91e147afe7eb7d267ed7d60aa364b5ad
Lucus L <lucus.lovell@gmail.com>
2016-06-28 20:23:29 +1000

    updated scripts folder name

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,6 +2,8 @@ var game = new Phaser.Game(600,400, Phaser.AUTO);
 
 //adding state 1 to our game 
 game.state.add('state1', demo.state1);
+game.state.add('state2', demo.state1);
+
 
 //setting state 1 to be the starting state
 game.state.start('state1');
---
+++ Scripts/state2.js
@@ -0,0 +1,8 @@
+//seting the state in an empty fuction 
+demo.state2 = fuction() {};
+
+demo.state2.protype = {
+	preload: fuction(){},
+	create: fuction(){},
+	update: fuction(){}
+};
